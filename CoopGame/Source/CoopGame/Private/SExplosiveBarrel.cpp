0a477de2aedee7c68e44b320383e41cb5c41be38
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-05-21 09:00:40 +0100

    Fixed bug in healthindicator Craete explosive barrel

---
+++ CoopGame/Source/CoopGame/Private/SExplosiveBarrel.cpp
@@ -0,0 +1,53 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SExplosiveBarrel.h"
+#include "SHealthComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "PhysicsEngine/RadialForceComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+
+// Sets default values
+ASExplosiveBarrel::ASExplosiveBarrel()
+{
+
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASExplosiveBarrel::OnHealthChanged);
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetSimulatePhysics(true);
+	MeshComp->SetCollisionObjectType(ECC_PhysicsBody);
+	RootComponent = MeshComp;
+
+	RadialForceComp = CreateDefaultSubobject<URadialForceComponent>(TEXT("RadialForceComp"));
+	RadialForceComp->SetupAttachment(MeshComp);
+	RadialForceComp->Radius = 250;
+	RadialForceComp->bImpulseVelChange = true;
+	RadialForceComp->bAutoActivate = false; // Prevent component from ticking, and only use FireImpulse() Instad
+	RadialForceComp->bIgnoreOwningActor = true; // Ignore self
+
+	ExplosionImpulse = 400;
+
+}
+
+void ASExplosiveBarrel::OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (bExploded)
+	{
+		return;
+	}
+
+	if (Health <= 0.0f)
+	{
+		bExploded = true;
+		FVector BoostIntensity = FVector::UpVector * ExplosionImpulse;
+		MeshComp->AddImpulse(BoostIntensity, NAME_None, true);
+
+		//UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+		MeshComp->SetMaterial(0, ExplosionMaterial);
+		RadialForceComp->FireImpulse();
+	}
+}
+
+
---
+++ CoopGame/Source/CoopGame/Public/SExplosiveBarrel.h
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SExplosiveBarrel.generated.h"
+
+class USHealthComponent;
+class UStaticMeshComponent;
+class URadialForceComponent;
+class UParticleSystem;
+class UMaterialInterface;
+
+UCLASS()
+class COOPGAME_API ASExplosiveBarrel : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASExplosiveBarrel();
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USHealthComponent* HealthComp;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	URadialForceComponent* RadialForceComp;
+
+
+	UFUNCTION()
+	void OnHealthChanged(USHealthComponent* OwningHealthComp, float Health, float HealthDelta, const class UDamageType* DamageType, class AController* InstigatedBy, AActor* DamageCauser);
+
+	bool bExploded;
+
+	// Impulse appplied to the barrel mesh when it explodes to boost it up a little
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	float ExplosionImpulse;
+
+	// Particle to play when healt reached zero
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	UParticleSystem* ExplosionEffect;
+
+	// The material to replace the original on the mesh once exploded (a blakend version)
+	UPROPERTY(EditDefaultsOnly, Category = "FX")
+	UMaterialInterface* ExplosionMaterial;
+	
+};
