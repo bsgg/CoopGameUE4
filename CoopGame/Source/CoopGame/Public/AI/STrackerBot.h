8d2599719e1a709604a5244beaa47067e71fb437
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-05-29 08:59:47 +0100

    Include TrackerBot

--- CoopGame/Source/CoopGame/Public/AI/STrackerBot.h
+++ CoopGame/Source/CoopGame/Public/AI/STrackerBot.h
@@ -3,26 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "ExplosiveBarrel.generated.h"
+#include "GameFramework/Pawn.h"
+#include "STrackerBot.generated.h"
 
 UCLASS()
-class COOPGAME_API AExplosiveBarrel : public AActor
+class COOPGAME_API ASTrackerBot : public APawn
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AExplosiveBarrel();
+
+public:
+	// Sets default values for this pawn's properties
+	ASTrackerBot();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
 	
 };
