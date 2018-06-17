76f9c9ddf6941b0541b162bf8ea11dd89bb19bc7
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-06-17 11:32:46 +0100

    Power up speed

--- CoopGame/Source/CoopGame/Private/SPowerupActor.cpp
+++ CoopGame/Source/CoopGame/Private/SPowerupActor.cpp
@@ -37,9 +37,11 @@ void ASPowerupActor::OnTickPowerup()
 
 void ASPowerupActor::ActivatePowerup()
 {
+	OnActivated();
+
 	if (PowerupInterval > 0.0f)
 	{
-		GetWorldTimerManager().SetTimer(TimerHandler_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true, 0.0f);
+		GetWorldTimerManager().SetTimer(TimerHandler_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval, true);
 	}
 	else
 	{
