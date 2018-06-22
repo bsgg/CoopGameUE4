d011c80a0cb632d79dd5d5fbf86302d516efb43d
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-06-22 08:56:42 +0100

    Spawn bots query

--- CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
+++ CoopGame/Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -72,7 +72,7 @@ FVector ASTrackerBot::GetNextPathPoints()
 
 	UNavigationPath* NavPath = UNavigationSystem::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
 
-	if (NavPath->PathPoints.Num() > 1)
+	if (NavPath && NavPath->PathPoints.Num() > 1)
 	{
 		return NavPath->PathPoints[1];
 	}
