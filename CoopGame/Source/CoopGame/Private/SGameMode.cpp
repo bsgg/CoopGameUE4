05e2920da6303339cdfee98849d9410531687378
Mart-PC\Mart <beatriz.s.g86@gmail.com>
2018-07-06 08:47:38 +0100

    Added cover challenge

--- CoopGame/Source/CoopGame/Private/SGameMode.cpp
+++ CoopGame/Source/CoopGame/Private/SGameMode.cpp
@@ -45,6 +45,8 @@ void ASGameMode::PrepareForNextWave()
 	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, TimeBetweenWaves, false);
 
 	SetWaveState(EWaveState::WaitingToStart);
+
+	RestartDeadPlayers();
 }
 
 
@@ -127,6 +129,19 @@ void ASGameMode::SetWaveState(EWaveState NewState)
 	}
 }
 
+
+void ASGameMode::RestartDeadPlayers()
+{
+	for (FConstPlayerControllerIterator It = GetWorld()->GetPlayerControllerIterator(); It; ++It)
+	{
+		APlayerController* PC = It->Get();
+		if (PC && PC->GetPawn() == nullptr)
+		{
+			RestartPlayer(PC);
+		}
+	}
+
+}
 
 void ASGameMode::StartPlay()
 {
